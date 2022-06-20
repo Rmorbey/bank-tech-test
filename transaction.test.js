7a863a4de4ea49763cbe3a47eaafc433427ab844
Russell <russellmorbey@hotmail.co.uk>
2022-06-20 19:41:04 +0100

    Transaction Class. Refactored test structure.

--- transaction.test.js
+++ transaction.test.js
@@ -2,55 +2,51 @@ const Transaction = require('./transaction');
 
 describe('Transaction', () => {
   describe('deposit transaction', () => {
+    let transaction;
+
+    beforeEach(() => {
+      transaction = new Transaction('deposit', 1000, 1000, '22/06/2022')
+    }) 
+
     it('records the type of the transaction', () => {
-      let transaction = new Transaction('deposit', 1000, 1000, '20/06/2022')
-  
       expect(transaction.type).toBe('deposit')
     })
   
     it('records the amount of the transaction', () => {
-      let transaction = new Transaction('deposit', 1000, 1000, '20/06/2022')
-  
       expect(transaction.amount).toBe(1000)
     })
   
     it('records the balance of the transaction', () => {
-      let transaction = new Transaction('deposit', 1000, 1000, '20/06/2022')
-  
       expect(transaction.balance).toBe(1000)
     })
   
     it('records the date of the transaction', () => {
-      let transaction = new Transaction('deposit', 1000, 1000, '20/06/2022')
-  
-      expect(transaction.date).toBe('20/06/2022')
+      expect(transaction.date).toBe('22/06/2022')
     })
 
   })
 
   describe('withdraw transaction', () => {
+    let transaction;
+
+    beforeEach(() => {
+      transaction = new Transaction('withdraw', 500, 2000, '23/06/2022')
+    }) 
+    
     it('records the type of the transaction', () => {
-      let transaction = new Transaction('withdraw', 500, 2000, '21/06/2022')
       expect(transaction.type).toBe('withdraw')
     })
+
+    it('records the amount of the transaction', () => {
+      expect(transaction.amount).toBe(500)
+    })
+  
+    it('records the balance of the transaction', () => {
+      expect(transaction.balance).toBe(2000)
+    })
+  
+    it('records the date of the transaction', () => {
+      expect(transaction.date).toBe('23/06/2022')
+    })
   })
-
-  it('records the amount of the transaction', () => {
-    let transaction = new Transaction('withdraw', 500, 2000, '21/06/2022')
-
-    expect(transaction.amount).toBe(500)
-  })
-
-  it('records the balance of the transaction', () => {
-    let transaction = new Transaction('withdraw', 500, 2000, '21/06/2022')
-
-    expect(transaction.balance).toBe(2000)
-  })
-
-  it('records the date of the transaction', () => {
-    let transaction = new Transaction('withdraw', 500, 2000, '21/06/2022')
-
-    expect(transaction.date).toBe('21/06/2022')
-  })
-
 })
