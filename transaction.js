9d2d46310ee3e1db399126a2a51990357af3e891
Russell <russellmorbey@hotmail.co.uk>
2022-06-20 19:24:17 +0100

    Transaction Class. Date test Green.

--- transaction.js
+++ transaction.js
@@ -3,6 +3,7 @@ class Transaction {
     this.type = 'deposit';
     this.amount = 1000;
     this.balance = 1000;
+    this.date = '20/06/2022';
   }
 }
 
--- transaction.test.js
+++ transaction.test.js
@@ -19,4 +19,9 @@ describe('Transaction', () => {
     expect(transaction.balance).toBe(1000)
   })
 
+  it('records the date of the transaction', () => {
+    let transaction = new Transaction('deposit', 1000, 1000, '20/06/2022')
+
+    expect(transaction.date).toBe('20/06/2022')
+  })
 })
