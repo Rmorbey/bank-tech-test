f1fd8f78b9cc297c3d779dad987c875608195d8d
Russell <russellmorbey@hotmail.co.uk>
2022-06-20 17:24:43 +0100

    Refactored tests to get rid of redundant tests.

--- bankAccount.test.js
+++ bankAccount.test.js
@@ -1,20 +1,18 @@
 const BankAccount = require('./bankAccount');
 
 describe('BankAccount', () => {
+  let bank;
+
+  beforeEach(() => {
+    bank = new BankAccount();
+  }) 
+
   it('Bank account starts with a balance of 0', () => {
-    let bank = new BankAccount();
     expect(bank.getBalance()).toBe(0)
   })
 
-  describe(`.depositMoney`, () => {
-    it('User can deposit money into the bank account', () => {
-      let bank = new BankAccount();
-  
-      expect(bank.depositMoney(20)).toBe(20)
-    });
-
+  describe(`Account interactions`, () => {
     it('User can deposit money multiple times into the bank account and show the balance', () => {
-      let bank = new BankAccount();
 
       bank.depositMoney(100);
       bank.depositMoney(100);
@@ -22,40 +20,28 @@ describe('BankAccount', () => {
 
       expect(bank.getBalance()).toBe(300)
     });
-  })
-
-  describe(`.withdrawMoney`, () => {
-    it('User can withdraw money from the bank account', () => {
-      let bank = new BankAccount();
-
-      bank.balance = 20;
-  
-      expect(bank.withdrawMoney(20)).toBe(0)
-    })
 
     it('User can withdraw money multiple times from the bank account and show the balance', () => {
-      let bank = new BankAccount();
-
+  
       bank.balance = 350
-
+  
       bank.withdrawMoney(100);
       bank.withdrawMoney(100);
       bank.withdrawMoney(100);
   
       expect(bank.getBalance()).toBe(50)
-    })
-  })
+    });
 
-  it('User can deposit and withdraw money and the balance will be correct', () => {
-    let bank = new BankAccount();
-
-    bank.balance = 150;
-
-    bank.withdrawMoney(100);
-    bank.depositMoney(1000);
-    bank.depositMoney(500);
-    bank.withdrawMoney(750);
-
-    expect(bank.getBalance()).toBe(800)
-  })
+    it('User can deposit and withdraw money and the balance will be correct', () => {
+  
+      bank.balance = 150;
+  
+      bank.withdrawMoney(100);
+      bank.depositMoney(1000);
+      bank.depositMoney(500);
+      bank.withdrawMoney(750);
+  
+      expect(bank.getBalance()).toBe(800)
+    });
+  });
 })
